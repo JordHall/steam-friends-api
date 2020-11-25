3cdfa5b987c70007f77cc8036feb4e2b1b112525
Jordan Hall <jordanhall@talktalk.net>
2020-11-25 00:05:16 +0000

    Initialise steam game api project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    const api = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=391C66AEE33C44E3C36C283651E05BEA&steamid=76561198040298609&include_appinfo=1&format=json"
+    const cors = "https://cors-anywhere.herokuapp.com/"
+    fetch(cors + api)
+    .then(res => res.json())
+    .then((json) => {
+      this.setState({
+        isLoaded: true,
+        items: json.response.games
+      })
+    })
+  }
+
+  render() {
+    const { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+    else {
+      items.sort(function(a,b) {
+        return b.playtime_forever - a.playtime_forever;
+      })
+      return (
+        <div className="App">
+            <ul>
+              {items.map(item => (
+                <li key={item.appid}>
+                  {item.name} | Playtime: {Math.round(item.playtime_forever/60)} hours
+                </li>
+              ))}
+            </ul>
+        </div>
+      )
+    }
+  }
 }
 
 export default App;
