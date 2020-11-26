361447dd463764dbd89dff684a721dc4e55cf057
Jordan Hall <jordanhall@talktalk.net>
2020-11-26 20:47:45 +0000

    Added primary functionality to app.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ class App extends Component {
     super(props);
     this.state = {
       items: [],
-      isLoaded: false
+      isLoaded: false,
+      items2: [],
+      isLoaded2: false,
+      combArray: []
     }
   }
 
   componentDidMount() {
     const api = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=391C66AEE33C44E3C36C283651E05BEA&steamid=76561198040298609&include_appinfo=1&format=json"
+    const api2 = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=391C66AEE33C44E3C36C283651E05BEA&steamid=76561198054970492&include_appinfo=1&format=json"
     const cors = "https://cors-anywhere.herokuapp.com/"
+    
     fetch(cors + api)
     .then(res => res.json())
     .then((json) => {
@@ -21,22 +26,41 @@ class App extends Component {
         items: json.response.games
       })
     })
+    fetch(cors + api2)
+    .then(res => res.json())
+    .then((json) => {
+      this.setState({
+        isLoaded2: true,
+        items2: json.response.games
+      })
+    })
   }
 
   render() {
-    const { isLoaded, items } = this.state;
-
-    if (!isLoaded) {
+    const { isLoaded, isLoaded2, items, items2 } = this.state;
+    if (!isLoaded && !isLoaded2) {
       return <div>Loading...</div>
     }
     else {
-      items.sort(function(a,b) {
+      console.log(items)
+      console.log(items2)
+      let newCombArray = []
+      items.forEach( (item) => {
+        items2.forEach((item2) => {
+          if (item2.appid === item.appid) {
+            item.playtime_forever = (item.playtime_forever + item2.playtime_forever)/2
+            newCombArray.push(item)
+          }
+        })
+      })
+      newCombArray.sort(function(a,b) {
         return b.playtime_forever - a.playtime_forever;
       })
+      console.log(newCombArray)
       return (
         <div className="App">
             <ul>
-              {items.map(item => (
+              {newCombArray.map(item => (
                 <li key={item.appid}>
                   {item.name} | Playtime: {Math.round(item.playtime_forever/60)} hours
                 </li>
