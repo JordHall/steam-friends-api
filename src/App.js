ca9cdf7b14f3f2031a140c69d813403a35fa7e15
Jordan Hall <jordanhall@talktalk.net>
2020-11-27 01:37:45 +0000

    Added bootstrap and polished UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 class App extends Component {
 
@@ -15,7 +16,7 @@ class App extends Component {
 
   componentDidMount() {
     const api = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=391C66AEE33C44E3C36C283651E05BEA&steamid=76561198040298609&include_appinfo=1&format=json"
-    const api2 = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=391C66AEE33C44E3C36C283651E05BEA&steamid=76561198054970492&include_appinfo=1&format=json"
+    const api2 = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=391C66AEE33C44E3C36C283651E05BEA&steamid=76561198165907050&include_appinfo=1&format=json"
     const cors = "https://cors-anywhere.herokuapp.com/"
     
     fetch(cors + api)
@@ -36,6 +37,10 @@ class App extends Component {
     })
   }
 
+  createSteamImage(id, hash) {
+    return "http://media.steampowered.com/steamcommunity/public/images/apps/" + id + "/" + hash +".jpg"
+  }
+
   render() {
     const { isLoaded, isLoaded2, items, items2 } = this.state;
     if (!isLoaded && !isLoaded2) {
@@ -45,7 +50,7 @@ class App extends Component {
       console.log(items)
       console.log(items2)
       let newCombArray = []
-      items.forEach( (item) => {
+      items.forEach((item) => {
         items2.forEach((item2) => {
           if (item2.appid === item.appid) {
             item.playtime_forever = (item.playtime_forever + item2.playtime_forever)/2
@@ -56,16 +61,30 @@ class App extends Component {
       newCombArray.sort(function(a,b) {
         return b.playtime_forever - a.playtime_forever;
       })
+      let maxHours = newCombArray[0]
       console.log(newCombArray)
       return (
-        <div className="App">
-            <ul>
+        <div class="container">
+          <div class="col text-center">
+            <h1>Shared steam games</h1>
+            <h4>By average playtime</h4>
+          </div>
+          <div class="col mx-auto">
               {newCombArray.map(item => (
-                <li key={item.appid}>
-                  {item.name} | Playtime: {Math.round(item.playtime_forever/60)} hours
-                </li>
+                <div class="row m-2" key={item.appid}>
+                  <img src={this.createSteamImage(item.appid,item.img_logo_url)} class="img rounded h-50" alt="Logo"></img>
+                  <div class="col">
+                    <h2>{item.name}</h2>
+                  </div>
+                  <div class="col-2">
+                    <span class="badge badge-info">{Math.round(item.playtime_forever/60)} hrs</span>
+                    <div class="progress">
+                      <div class="progress-bar" style={{width:((item.playtime_forever/maxHours.playtime_forever)*100)}}></div>
+                    </div>
+                  </div>
+                </div>
               ))}
-            </ul>
+          </div>
         </div>
       )
     }
